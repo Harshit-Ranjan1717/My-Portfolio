767cd6edc14080175076ac5ec642603a7d4ed7cc
Harshit-Ranjan1717 <dankcoder17@gmail.com>
2024-02-14 14:55:42 +0530

    added css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,21 +6,21 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="w-full text-white  h-screen bg-gradient-to-b from-black via-black to-gray-800"
+      className="w-full pt-10 text-white  h-screen bg-gradient-to-b from-black via-black to-gray-800"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-6 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl sm:w-[600px] font-bold">
             I am a  <span className="text-red-600">Full Stack </span>Developer
           </h2>
-          <p className="  text-gray-400 py-4 max-w-md">
+          <p className="  text-gray-400 pt-8 max-w-md">
             {" "}
             Lorem ipsum dolor sit ae, mollitia reprehenderit illo rem. Iusto
            a nobis aliquam asperiores non magni
            a nobis aliquam asperiores non magni
             quae fugiat.
           </p>
-          <div>
+          <div className="mt-10">
             <Link to="portfolio" smooth duration={500} className=" cursor-pointer group w-fit px-6 py-3 my-3 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
               Portfolio{" "}
               <span className="pl-2 group-hover:rotate-90 duration-300">
